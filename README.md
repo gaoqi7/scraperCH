57d014ec52b7174c8a45dfcb925a18219bb0193b
Rick Gao <ricky@Qis-MacBook-Pro.local>
2020-01-09 09:45:11 -0800

    add README and some comments in code file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# scraperCH
+## The project is a web scraper for coursehunter.net; I use this project to learn [nodejs](https://nodejs.org), it is only used **For Educational Purposes**.
+
+## How it works
+
+1. Use node buildin function `fs.mkdirSync()` create a folder _download_ to storage the downloaded file.
+2. Use package **`axios`** to download the HTML content and cheery pick the data from it with package **cheerio**.
+3. Write the downloaded contents to file.
+
+## Thanks
+
+1. @bertyhell for the great [solution](https://github.com/caolan/async/issues/669#issuecomment-358962580) about `async.map` [index issue](https://github.com/caolan/async/issues/669)
+2. Good explanation about the [stream](https://www.freecodecamp.org/news/node-js-streams-everything-you-need-to-know-c9141306be93/)
--- index.js
+++ index.js
@@ -2,22 +2,27 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 const async = require("async");
 const fs = require("fs");
-
+// Make a folder "First"
+// That is why I need use mkdirSync
 fs.mkdirSync("download", { recursive: true }, err => {
   if (err) throw err;
 });
+// Parse the HTML file and cherry pick data from it
+// We need file name and file url in this case
 axios
   .get("https://coursehunter.net/course/es6-for-everyone")
   .then(response => {
     const $ = cheerio.load(response.data);
     const result = [];
     $(".lessons-item").map(function(el) {
+      // Pick the URL
       let u = $(this)
         .children()
         .filter(function(child) {
           return $(this).attr("itemprop") === "contentUrl";
         })
         .attr("href");
+      // Pick the name
       let n = $(this)
         .children()
         .filter(function(child) {
@@ -26,17 +31,21 @@ axios
         .text();
       result.push({ name: n, url: u });
     });
-    console.log(result);
     return result;
   })
   .then(contents => {
+    // I want to include the index nmuber as part of file name for sorting purpose
+    // But package async doesn't map the index
+    // I have to use Object.keys and ask async to map the index
     let contentsIndexes = Object.keys(contents);
     async.map(contentsIndexes, contentIndex => {
       axios
+        // The option responseType is required
         .get(contents[contentIndex].url, { responseType: "stream" })
         .then(videoFile =>
           videoFile.data.pipe(
             fs.createWriteStream(
+              //file name
               `./download/${parseInt(contentIndex) + 1}.${
                 contents[contentIndex].name
               }.mp4`
